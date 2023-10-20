226075ef77b2aa2a925d17683d896d176184e135
Siddharth Hota <siddharth.hota@incture.com>
2023-10-20 16:25:48 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="">
+      <h1 className="text-red-500">je Autmation</h1>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
