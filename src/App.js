8ee6799db24bc66d4886b3249fcafda35845d50e
Swastik Mohapatra <swastik.mohapatra@incture.com>
2023-10-26 16:43:15 +0530

    Table added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import MTInputs from "./components/MTInputs";
+import MJTable from "./components/MJTable";
 
 function App() {
   return (
-    <div className="">
-      <h1 className="text-red-500">je Autmation</h1>
-      <h2>My file</h2>
-    </div>
+    <>
+      <NavBar />
+      <div className="flex mx-4 font-roborto">
+
+       <p className="font-bold text-gray-500">Note:</p> 
+      <p className="ml-2 text-gray-500">
+        Please Upload the template to start the Manual Journal process. If you
+        require the template , please choose the required process and/or
+        sub-process and click on Download Template. ***Line items entered can
+        not exceed 999***
+      </p>
+      </div>
+      <MTInputs/>
+      <MJTable/>
+    </>
   );
 }
 
